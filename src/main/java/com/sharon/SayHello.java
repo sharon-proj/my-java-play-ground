48cd68ce6cf5a0a94e5bd4bfe5fbe18ac2e24354
John Ruben <john.ruben@bhnetwork.com>
2019-08-22 09:03:10 -0700

    Added changes for test and created new class

---
+++ src/main/java/com/sharon/SayHello.java
@@ -0,0 +1,11 @@
+package com.sharon;
+
+public class SayHello {
+    public static void main(String[] args){
+        System.out.println("Hello first code");
+    }
+
+    public String hello(String name){
+        return "Hello "+name;
+    }
+}
